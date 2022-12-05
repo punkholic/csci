b1fd2624a6a6ee188a3afc21fd5ba400d9bb0cde
punkholic <ashtom@github.com>
2022-12-05 14:46:42 +0545

    first commit

---
+++ firstday.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main(){
+    // \t provides tab while printing, a tab is combination of 4 space character
+    // \n provides new line while printing, which means it breaks the line
+    printf("Hello\tWorld");
+    printf("Hello\nWorld");
+}
