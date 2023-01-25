5abbc61841f5d5674594418114274a7df4afc218
punkholic <nirajghimirexyz@gmail.com>
2023-01-25 16:46:53 +0545

    test

--- java_programming/Test.java
+++ java_programming/Test.java
@@ -1,6 +1,46 @@
-public class Test {
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
+/*
+ * class ClassName{
+ *  // class member
+ * }
+ * 
+ */
+class Animal{
+    private int leg;
+    private int eye;
+    private String breed;
 
+   
+    public String getBreed() {
+        return breed;
+    }
+    public int getEye() {
+        return eye;
+    }
+    public int getLeg() {
+        return leg;
+    }
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+    public void setEye(int eye) {
+        this.eye = eye;
+    }
+    public void setLeg(int leg) {
+        this.leg = leg;
+    }
+    void printInfo(){
+        System.out.println("leg = " + leg + ", eye = "+ eye + ", breed = " + breed);
+    }
+}
+
+
+public class Test {
+    public static void main(String[] args) {
+        // ClassName objectName = new ClassName();
+        Animal animal1 = new Animal();
+        animal1.setBreed("bird");
+        animal1.setLeg(2);
+        animal1.setEye(2);
+        animal1.printInfo();
+    }
 }
